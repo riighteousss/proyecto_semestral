e91d08ba58da85cd4b5e58bba133c1397355ede6
ryokasb <dant.riverar@duocuc.cl>
2025-06-22 17:29:01 -0400

    documentacion completa notificaciones

--- gestionnotificaciones/src/main/java/com/example/gestionnotificaciones/controller/NotificacionController.java
+++ gestionnotificaciones/src/main/java/com/example/gestionnotificaciones/controller/NotificacionController.java
@@ -2,18 +2,34 @@ package com.example.gestionnotificaciones.controller;
 
 import com.example.gestionnotificaciones.model.Notificacion;
 import com.example.gestionnotificaciones.service.NotificacionService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
+@Tag(name = "notificaciones",description = "operaciones relacionadas con la gestión de notificaciones")
 @RestController
 @RequestMapping("/api/v1")
 public class NotificacionController {
     @Autowired
     private NotificacionService notificacionService;
 
+      //endpoint para obtener todas las notificaciones
+     @Operation(summary = "obtener todas las notificaciones", description = "devuelve una lista con todas las notificaciones")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Lista de notificaciones obtenida correctamente",
+            content = @Content(schema = @Schema(implementation = Notificacion.class))),
+        @ApiResponse(responseCode = "204", description = "No hay notificaciones registradas")
+    })
     @GetMapping("/notificaciones")
     public ResponseEntity<List<Notificacion>> obtenerNotificaciones() {
         List<Notificacion> notificaciones = notificacionService.buscarTodas();
@@ -21,7 +37,14 @@ public class NotificacionController {
             ResponseEntity.noContent().build() : 
             ResponseEntity.ok(notificaciones);
     }
-
+    //endpoint para obtener todas las notificaciones de un usuario
+     @Operation(summary = "obtener todas las notificaciones de un usuario", description = "devuelve una lista con todas las notificaciones de un usuario")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Lista de notificaciones obtenida correctamente",
+            content = @Content(schema = @Schema(implementation = Notificacion.class))),
+             @ApiResponse(responseCode = "204", description = "El usuario no tiene notificaciones"),
+        @ApiResponse(responseCode = "400", description = "ID de usuario inválido o error en la solicitud")
+    })
     @GetMapping("/notificaciones/usuario/{usuarioId}")
     public ResponseEntity<?> obtenerPorUsuario(@PathVariable Long usuarioId) {
         try {
@@ -33,7 +56,13 @@ public class NotificacionController {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
-
+    //endpoint para crear una notificacion 
+    @Operation(summary = "Crear una nueva notificación", description = "Permite crear una nueva notificación para un usuario específico.")
+@ApiResponses(value = {
+    @ApiResponse(responseCode = "201", description = "Notificación creada correctamente",
+        content = @Content(schema = @Schema(implementation = Notificacion.class))),
+    @ApiResponse(responseCode = "400", description = "Error en la solicitud. datos inválidos.")
+})
     @PostMapping("/notificaciones")
     public ResponseEntity<?> crearNotificacion(@RequestBody Notificacion notificacion) {
         try {
