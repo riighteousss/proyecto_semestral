b844723a7a3161bf3c4c075e40a23d3a918581af
ryokasb <dant.riverar@duocuc.cl>
2025-06-22 01:54:53 -0400

    documentacion completa equipos
    documentacion completa en
    controller y model.

--- equipos/src/main/java/com/example/equipos/model/equipo.java
+++ equipos/src/main/java/com/example/equipos/model/equipo.java
@@ -1,5 +1,6 @@
 package com.example.equipos.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -15,20 +16,28 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "modelo de equipo(dispositivos de un usuario)")
 public class equipo {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(description = "ID autoincrementable del equipo")
     private Long id;
+
     @Column(nullable = false)
-    //llave foranea de idusuario para validar a qn pertenece el equipo
+    @Schema(description = "ID del usuario propietario del equipo (llave foránea)", required = true)
     private Long idusuario;
-    //telefono, tablet, notebook, pc ..etc
+
     @Column()
+    @Schema(description = "Tipo de dispositivo (Ej: teléfono, tablet, notebook, PC)", required = true)
     private String tipodispositivo;
+
     @Column()
+    @Schema(description = "Marca del dispositivo (Ej: Dell, Samsung, Apple)", required = true)
     private String marca;
+
     @Column()
+    @Schema(description = "Modelo del dispositivo (Ej: iphone 13, Galaxy S21, iPad)", required = true)
     private String modelo;
 
 }
--- equipos/src/test/java/com/example/equipos/service/equiposerviceTest.java
+++ equipos/src/test/java/com/example/equipos/service/equiposerviceTest.java
@@ -1,10 +1,20 @@
 package com.example.equipos.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.example.equipos.model.equipo;
 import com.example.equipos.repository.EquipoRepository;
 
 @ExtendWith(MockitoExtension.class)
@@ -15,6 +25,19 @@ public class equiposerviceTest {
     @InjectMocks
     private EquipoService service;
 
-    
+    @Test
+    public void testBuscarquipos_RetornaLista() {
+        List<equipo> listaMock = Arrays.asList(
+            new equipo(1L, 100L, "Laptop", "Dell", "XPS"),
+            new equipo(2L, 101L, "Celular", "Samsung", "S21")
+        );
 
+        when(equiporepository.findAll()).thenReturn(listaMock);
+
+        List<equipo> resultado = service.buscarquipos();
+
+        assertEquals(2, resultado.size());
+        verify(equiporepository, times(1)).findAll();
+    }
+   
 }
