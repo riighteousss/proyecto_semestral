0c5cbeee14b5e3aa5cdddfc0aca2edc632f6e886
ryokasb <dant.riverar@duocuc.cl>
2025-06-22 18:19:44 -0400

    documentacion completa inventario

--- gestioninventario/src/main/java/com/example/gestioninventario/controller/InventarioController.java
+++ gestioninventario/src/main/java/com/example/gestioninventario/controller/InventarioController.java
@@ -2,6 +2,13 @@ package com.example.gestioninventario.controller;
 
 import com.example.gestioninventario.model.inventario;
 import com.example.gestioninventario.service.InventarioService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +26,12 @@ public class InventarioController {
     private InventarioService inventarioService;
     
     // Obtener todos los productos activos
+    @Operation(summary = "Obtener productos activos", description = "Devuelve una lista con todos los productos activos en inventario")
+          @ApiResponses(value = {
+    @ApiResponse(responseCode = "200", description = "Lista de productos obtenida correctamente",
+        content = @Content(schema = @Schema(implementation = inventario.class))),
+    @ApiResponse(responseCode = "204", description = "No hay productos activos")
+})
     @GetMapping("/productos")
     public ResponseEntity<List<inventario>> obtenerProductos() {
         List<inventario> productos = inventarioService.buscarinventarios();
@@ -28,6 +41,12 @@ public class InventarioController {
     }
     
     // Obtener todos los productos (incluyendo inactivos)
+    @Operation(summary = "Obtener todos los productos", description = "Devuelve una lista con todos los productos, incluyendo los inactivos")
+@ApiResponses(value = {
+    @ApiResponse(responseCode = "200", description = "Lista de productos obtenida correctamente",
+        content = @Content(schema = @Schema(implementation = inventario.class))),
+    @ApiResponse(responseCode = "204", description = "No hay productos registrados")
+})
     @GetMapping("/productos/todos")
     public ResponseEntity<List<inventario>> obtenerTodosLosProductos() {
         List<inventario> productos = inventarioService.buscarTodos();
@@ -37,6 +56,13 @@ public class InventarioController {
     }
     
     // Obtener producto por ID
+       @Operation(summary = "Obtener producto por ID", description = "Devuelve un producto por su ID")
+    @ApiResponses(value = {
+    @ApiResponse(responseCode = "200", description = "Producto obtenido correctamente",
+        content = @Content(schema = @Schema(implementation = inventario.class))),
+    @ApiResponse(responseCode = "404", description = "Producto no encontrado"),
+    @ApiResponse(responseCode = "500", description = "Error interno en el servidor")
+  })
     @GetMapping("/productos/{id}")
     public ResponseEntity<?> obtenerProductoPorId(@PathVariable Long id) {
         try {
@@ -50,6 +76,12 @@ public class InventarioController {
     }
     
     // Obtener productos por nombre
+      @Operation(summary = "Obtener productos por nombre", description = "Devuelve una lista de productos que coinciden con el nombre")
+      @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "Productos obtenidos correctamente",
+        content = @Content(schema = @Schema(implementation = inventario.class))),
+    @ApiResponse(responseCode = "204", description = "No se encontraron productos con ese nombre")
+})
     @GetMapping("/productos/nombre/{nombre}")
     public ResponseEntity<List<inventario>> obtenerProductosPorNombre(@PathVariable String nombre) {
         List<inventario> productos = inventarioService.buscarPorNombre(nombre);
@@ -59,6 +91,12 @@ public class InventarioController {
     }
     
     // Obtener productos por categoría
+    @Operation(summary = "Obtener productos por categoría", description = "Devuelve una lista de productos que pertenecen a la categoría")
+@ApiResponses(value = {
+    @ApiResponse(responseCode = "200", description = "Productos obtenidos correctamente",
+        content = @Content(schema = @Schema(implementation = inventario.class))),
+    @ApiResponse(responseCode = "204", description = "No se encontraron productos en esa categoria")
+})
     @GetMapping("/productos/categoria/{categoria}")
     public ResponseEntity<List<inventario>> obtenerProductosPorCategoria(@PathVariable String categoria) {
         List<inventario> productos = inventarioService.buscarPorCategoria(categoria);
@@ -68,6 +106,12 @@ public class InventarioController {
     }
     
     // Obtener productos con stock bajo
+    @Operation(summary = "Obtener productos con stock bajo", description = "Devuelve una lista de productos los cuales su stock es menor al mínimo")
+@ApiResponses(value = {
+    @ApiResponse(responseCode = "200", description = "Productos con stock bajo obtenidos correctamente",
+        content = @Content(schema = @Schema(implementation = inventario.class))),
+    @ApiResponse(responseCode = "204", description = "No hay productos con stock bajo")
+})
     @GetMapping("/productos/stock-bajo")
     public ResponseEntity<List<inventario>> obtenerProductosStockBajo() {
         List<inventario> productos = inventarioService.buscarStockBajo();
@@ -77,6 +121,12 @@ public class InventarioController {
     }
     
     // Crear nuevo producto
+    @Operation(summary = "Crear nuevo producto", description = "Permite registrar un nuevo producto en el inventario")
+@ApiResponses(value = {
+    @ApiResponse(responseCode = "201", description = "Producto creado correctamente",
+        content = @Content(schema = @Schema(implementation = inventario.class))),
+    @ApiResponse(responseCode = "400", description = "Error en la solicitud. Datos inválidos.")
+})
     @PostMapping("/productos")
     public ResponseEntity<?> crearProducto(@RequestBody inventario producto) {
         try {
@@ -96,6 +146,12 @@ public class InventarioController {
     }
     
     // Modificar producto completo
+    @Operation(summary = "Modificar producto", description = "Permite modificar completamente los datos de un producto existente")
+    @ApiResponses(value = {
+    @ApiResponse(responseCode = "200", description = "Producto modificado correctamente",
+        content = @Content(schema = @Schema(implementation = inventario.class))),
+    @ApiResponse(responseCode = "400", description = "Error en la solicitud. Datos inválidos.")
+})
     @PutMapping("/productos/{id}")
     public ResponseEntity<?> modificarProducto(
             @PathVariable Long id,
@@ -118,6 +174,13 @@ public class InventarioController {
     }
     
     // Actualizar solo el stock
+    @Operation(summary = "Actualizar stock de producto", description = "Permite actualizar del stock de un producto")
+@ApiResponses(value = {
+    @ApiResponse(responseCode = "200", description = "Stock actualizado correctamente",
+        content = @Content(schema = @Schema(implementation = inventario.class))),
+    @ApiResponse(responseCode = "400", description = "Error en la solicitud. Datos inválidos.")
+})
+
     @PutMapping("/productos/{id}/stock")
     public ResponseEntity<?> actualizarStock(
             @PathVariable Long id,
@@ -131,6 +194,12 @@ public class InventarioController {
     }
     
     // Agregar stock
+    @Operation(summary = "Agregar stock a un producto", description = "Permite agregar unidades al stock de un producto")
+@ApiResponses(value = {
+    @ApiResponse(responseCode = "200", description = "Stock agregado correctamente",
+        content = @Content(schema = @Schema(implementation = inventario.class))),
+    @ApiResponse(responseCode = "400", description = "Error en la solicitud. Datos inválidos.")
+})
     @PutMapping("/productos/{id}/stock/agregar")
     public ResponseEntity<?> agregarStock(
             @PathVariable Long id,
@@ -144,6 +213,12 @@ public class InventarioController {
     }
     
     // Reducir stock
+    @Operation(summary = "Reducir stock de un producto", description = "Permite reducir stock de un producto")
+@ApiResponses(value = {
+    @ApiResponse(responseCode = "200", description = "Stock reducido correctamente",
+        content = @Content(schema = @Schema(implementation = inventario.class))),
+    @ApiResponse(responseCode = "400", description = "Error en la solicitud. Datos inválidos.")
+})
     @PutMapping("/productos/{id}/stock/reducir")
     public ResponseEntity<?> reducirStock(
             @PathVariable Long id,
@@ -157,6 +232,12 @@ public class InventarioController {
     }
     
     // Reactivar producto
+    @Operation(summary = "Reactivar producto", description = "Permite reactivar un producto inactivado")
+@ApiResponses(value = {
+    @ApiResponse(responseCode = "200", description = "Producto reactivado correctamente",
+        content = @Content(schema = @Schema(implementation = inventario.class))),
+    @ApiResponse(responseCode = "400", description = "Error en la solicitud. Datos inválidos.")
+})
     @PutMapping("/productos/{id}/reactivar")
     public ResponseEntity<?> reactivarProducto(@PathVariable Long id) {
         try {
@@ -168,6 +249,12 @@ public class InventarioController {
     }
     
     // Eliminar producto (desactivar)
+     @Operation(summary = "Eliminar producto (desactivar)", description = "Permite eliminar (desactivar) un producto del inventario")
+  @ApiResponses(value = {
+    @ApiResponse(responseCode = "200", description = "Producto eliminado correctamente"),
+    @ApiResponse(responseCode = "400", description = "Error en la solicitud. Datos inválidos.")
+})
+
     @DeleteMapping("/productos/{id}")
     public ResponseEntity<?> eliminarProducto(@PathVariable Long id) {
         try {
--- gestioninventario/src/main/java/com/example/gestioninventario/model/inventario.java
+++ gestioninventario/src/main/java/com/example/gestioninventario/model/inventario.java
@@ -1,5 +1,6 @@
 package com.example.gestioninventario.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -13,34 +14,37 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "inventario")
+@Table(name = "Inventario")
 @Entity
+@Schema(description = "inventario")
 public class inventario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(description = "ID autoincrementable")
     private Long id;
     
+    @Schema(description = "nombre del objeto a guardar")
     @Column(nullable = false)
     private String nombre;
-    
+      @Schema(description = "descripcion general de lo que es")
     @Column(nullable = true)
     private String descripcion;
-    
+      @Schema(description = "cantidad disponible")
     @Column(nullable = false)
     private Integer stock = 0;
-    
+      @Schema(description = "cantidad minima disponible")
     @Column(nullable = true)
     private Integer stockMinimo = 0;
-    
+      @Schema(description = "categorizacion, indica que tipo de objeto es")
     @Column(nullable = true)
     private String categoria;
-    
+      @Schema(description = "unidad de medida/contabilizacion EJ: kg, piezas, unidades etc")
     @Column(nullable = true)
     private String unidadMedida; // unidad, pieza, kg, etc.
-    
+      @Schema(description = "precio del objeto")
     @Column(nullable = true)
     private Double precio;
-    
+      @Schema(description = "indica si el objeto esta activo o no para su uso.")
     @Column(nullable = false)
     private Boolean activo = true;
 }
