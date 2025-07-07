41977925c2709b9ba605b6d324d5ae4fd7092686
ryokasb <dant.riverar@duocuc.cl>
2025-07-06 20:44:27 -0400

    Update LoadDatabase.java

--- usuarios/src/main/java/com/example/usuarios/config/LoadDatabase.java
+++ usuarios/src/main/java/com/example/usuarios/config/LoadDatabase.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.example.usuarios.model.Rol;
+import com.example.usuarios.model.usuario;
 import com.example.usuarios.repository.RoleRepository;
 import com.example.usuarios.repository.UsuarioRepository;
 
@@ -12,20 +13,33 @@ import com.example.usuarios.repository.UsuarioRepository;
 public class LoadDatabase {
 
     @Bean
-    CommandLineRunner initDatabase(RoleRepository roleRepo, UsuarioRepository userRepo){
-        return args ->{
-            //si no hay registros en la tablas
-            if(roleRepo.count() == 0 && userRepo.count() == 0){
-                //insertar los roles por defecto
+    CommandLineRunner initDatabase(RoleRepository roleRepo, UsuarioRepository userRepo) {
+        return args -> {
+            
+            if (roleRepo.count() == 0 && userRepo.count() == 0) {
+                // Insertar roles
                 Rol admin = new Rol();
                 admin.setNombre("Administrador");
                 roleRepo.save(admin);
 
-                Rol usuario = new Rol();
-                usuario.setNombre("Usuario");
-                roleRepo.save(usuario);
+                Rol user = new Rol();
+                user.setNombre("Usuario");
+                roleRepo.save(user);
 
-                
+                // Insertar usuarios
+                usuario u1 = new usuario();
+                u1.setUsername("admin");
+                u1.setPassword("admin123"); // En producción deberías cifrar esto
+                u1.setCorreo("admin@example.com");
+                u1.setRol(admin);
+                userRepo.save(u1);
+
+                usuario u2 = new usuario();
+                u2.setUsername("juan");
+                u2.setPassword("juan123");
+                u2.setCorreo("juan@example.com");
+                u2.setRol(user);
+                userRepo.save(u2);
             }
         };
     }
